19c730517c0949369e4d9edd4460c88e20c6b1ca
theporchrat <theporchrat@gmail.com>
2014-09-09 17:25:43 -0400

    thinking about all the stuff

---
+++ test/dispatcher.js
@@ -0,0 +1,13 @@
+var chai  = require('chai')
+  , sinon = require("sinon")
+  , sinonChai = require("sinon-chai")
+  , _      = require('lodash')
+  , Container = require('../index')
+  , Clank = require('clank');
+
+chai.use(sinonChai);
+chai.should();
+
+it( 'should register the store', function(){
+  
+})
