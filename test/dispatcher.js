ae12a8281b5bafa7923df41ac1ab9c6438ce6343
theporchrat <theporchrat@gmail.com>
2014-09-21 11:43:48 -0400

    tests and batching for store state

--- test/dispatcher.js
+++ test/dispatcher.js
@@ -29,7 +29,7 @@ it( 'should register the store only once', function(){
 })
 
 
-it( 'should register store actions', function(done){
+it( 'should register store actions', function(){
   var dispatcher = new boutique.Dispatcher()
     , spyA = sinon.spy(),spyB = sinon.spy()
     , payload = {};
@@ -44,18 +44,13 @@ it( 'should register store actions', function(done){
   new Store(dispatcher)
 
   dispatcher.dispatch({ action: 'MY_ACTION', data: payload })
-    .then(function(){
-      return dispatcher.dispatch({ action: 'MY_ACTION_B', data: payload })
-    })
-    .then(function(){
-      spyA.should.have.been.calledOnce.and.calledWithExactly(payload)
-      spyB.should.have.been.calledOnce.and.calledWithExactly(payload)
+  dispatcher.dispatch({ action: 'MY_ACTION_B', data: payload })
 
-      done()
-    })
+  spyA.should.have.been.calledOnce.and.calledWithExactly(payload)
+  spyB.should.have.been.calledOnce.and.calledWithExactly(payload)
 })
 
-it( 'should not dispatch while dispatching', function(done){
+it( 'should not dispatch while dispatching', function(){
   var dispatcher = new boutique.Dispatcher()
     , payload = {};
 
@@ -69,36 +64,97 @@ it( 'should not dispatch while dispatching', function(done){
 
   new Store(dispatcher)
 
-  dispatcher.dispatch({ action: 'ACTION', data: payload })
-    .then(null, function(err){
-      console.log('hiiii')
-      done()
-    })
+  ;(function(){
+    dispatcher.dispatch({ action: 'ACTION', data: payload })
+  }).should.throw('Dispatcher.dispatch(...): Cannot dispatch in the middle of a dispatch.')
+  
 })
 
-it.only( 'should wait for others', function(done){
+it( 'should wait for others', function(){
   var dispatcher = new boutique.Dispatcher()
-    , spyA = sinon.spy()
-    , storeA;
+    , spyB = sinon.spy()
+    , storeB;
 
   var StoreA = boutique.Store.extend({
-    actions: [ listenFor('ACTION',spyA) ]
-  });
-
-  var StoreB = boutique.Store.extend({
     actions: [ 
       listenFor('ACTION', function(){
-        this.waitFor(storeA).then(function(){
-          console.log('made it')
-          done();
-
-        })
+        spyB.should.not.have.been.calledOnce
+        this.waitFor(storeB)
+        spyB.should.have.been.calledOnce
       })
     ]
   });
 
-  storeA = new StoreA(dispatcher)
-  new StoreB(dispatcher)
+  var StoreB = boutique.Store.extend({
+    actions: [ listenFor('ACTION', spyB) ]
+  });
+
+  new StoreA(dispatcher)
+  storeB = new StoreB(dispatcher)
+  
+  dispatcher.dispatch({ action: 'ACTION', data: {} })
+})
+
+
+it( 'should concat actions', function(){
+  var dispatcher = new boutique.Dispatcher()
+    , Base = boutique.Store.extend({ actions: [ listenFor('ACTION', _.noop) ] })
+    , MyStore = Base.extend({ actions: [ listenFor('ACTION_2', _.noop) ] });
+
+  
+  (new MyStore(dispatcher)).actions.length.should.equal(2)
+})
+
+it( 'should emit change events', function(){
+  var dispatcher = new boutique.Dispatcher()
+    , spy = sinon.spy()
+    , store = boutique.Store.create({ 
+        dispatcher: dispatcher
+      });
+
+  store.listen(spy)
+
+  store.emitChange()
+
+  spy.should.have.been.calledOnce
+})
+
+
+it( 'should batch set calls', function(done){
+  var dispatcher = new boutique.Dispatcher()
+    , spy = sinon.spy()
+    , store = boutique.Store.create({ dispatcher: dispatcher })
+    , timer;
+
+  store.listen(spy = sinon.spy(function(){
+    if(!timer)
+      timer = setTimeout(function(){
+        store.state.should.eql({ hi: 'oops', a: 'hi' })
+        spy.should.have.been.calledOnce
+        done()
+      }, 0)
+  }))
+
+  store._set({ hi: 'hello' })
+  store._set({ a: 'hi' })
+
+  store.state.should.eql({ hi: 'hello', a: 'hi' })
+  
+  store._set({ hi: 'oops' })
+
+  spy.should.not.have.been.called
+})
+
+it( 'should respond to multiple actions from a single handler', function(){
+  var dispatcher = new boutique.Dispatcher()
+    , spy = sinon.spy()
+    , store = boutique.Store.create({ 
+        dispatcher: dispatcher,
+        actions: [ listenFor('ACTION', 'ACTION_B', spy) ] 
+      });
 
   dispatcher.dispatch({ action: 'ACTION', data: {} })
+  dispatcher.dispatch({ action: 'ACTION_B', data: {} })
+
+  spy.should.have.been.calledTwice
 })
